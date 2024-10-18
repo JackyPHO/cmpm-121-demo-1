dc1cf6c1e4453fd59bf847841353604d8885babb
JackyPHO <100171161+JackyPHO@users.noreply.github.com>
2024-10-18 08:59:55 -0700

    Step 3 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,27 @@ document.title = gameName;
 const header = document.createElement("HIIIII");
 header.innerHTML = gameName;
 app.append(header);
+//Step 1: Adding a click button
 const button = document.createElement("button");
 button.className = "button";
 button.textContent = "👽";
 app.append(button);
 
+//Step 2: Adding a click score
 const score: HTMLDivElement = document.querySelector("#score")!;
 let counter = 0;
 const s = document.createElement("Score");
-button.addEventListener("click", function(){
+button.addEventListener("click", function () {
+  counter++;
+  s.textContent = counter + " Aliens Spawned";
+    score.append(s);
+});
+
+//Step 3: Add 1 every second on top of the click score
+function update(){
     counter++;
     s.textContent = counter + " Aliens Spawned";
-});
-score.append(s);
+    score.append(s);
+}
+setInterval(update, 1000);
+
