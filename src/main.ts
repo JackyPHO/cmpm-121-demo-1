021be985913e3c3772f96ea4547928665f6a4f59
JackyPHO <100171161+JackyPHO@users.noreply.github.com>
2024-10-18 14:20:31 -0700

    Step 6 Complete

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ const b1 = document.createElement("button");
 b1.className = "button";
 b1.textContent = "👽";
 app.append(b1);
+let mul = 1;
 
 //Step 2: Adding a click score
 const score: HTMLDivElement = document.querySelector("#score")!;
@@ -29,39 +30,95 @@ let last = performance.now();
 function update(timestamp: number) {
   const delta = timestamp - last;
   last = timestamp;
-  const fps = 1000 / delta;
-  counter += 1 / fps;
+  const fps = (1000 / delta);
+  counter = (counter + mul / fps);
   s.textContent = counter.toFixed(5) + " Aliens";
   score.append(s);
   requestAnimationFrame(update);
 }
 requestAnimationFrame(update);
 
-//Step 5: Growth Rate Button
+//Step 6: Multiple upgrades and status
 const grow: HTMLDivElement = document.querySelector("#grow")!;
+const shop: HTMLDivElement = document.querySelector("#shop")!;
+
+const p1 = document.createElement("Price1");
+const p2 = document.createElement("Price2");
+const p3 = document.createElement("Price3");
+let q1 = 0;
+let q2 = 0;
+let q3 = 0;
+p1.innerText = "-10 Aliens = +0.1 Aliens/sec: \n";
+p2.innerText = "-100 Aliens = +2.0 Aliens/sec: \n";
+p3.innerText = "-1000 Aliens = +50 Aliens/sec: \n";
+
+const g = document.createElement("Growth");
+g.textContent = "Growth Rate : " + mul.toFixed(1) + " Aliens/sec";
+shop.append(p1);
+shop.append(p2);
+shop.append(p3);
+shop.append(g);
+
+
 const b2 = document.createElement("button");
 b2.className = "button";
-b2.textContent = "💲";
-let mul = 0;
-grow.append(b2);
-const g = document.createElement("Score");
-b2.disabled = true;
-function check(){
-    if(counter<10){
+b2.textContent = "🚀";
+grow.appendChild(b2);
+b2.addEventListener("click", function () {
+    mul += 0.1;
+    counter -= 10;
+    q1++;
+    p1.innerText = "-10 Aliens = +0.1 Aliens/sec: " + q1 + "\n";
+    updateText()
+});
+
+
+const b3 = document.createElement("button");
+b3.className = "button";
+b3.textContent = "🛸";
+grow.appendChild(b3);
+b3.addEventListener("click", function () {
+    mul += 2;
+    counter -= 100;
+    q2++;
+    p2.innerText = "-100 Aliens = +2.0 Aliens/sec: " + q2 + "\n";
+    updateText()
+});
+
+const b4 = document.createElement("button");
+b4.className = "button";
+b4.textContent = "👾";
+grow.appendChild(b4);
+b4.addEventListener("click", function () {
+    mul += 50;
+    counter -= 100;
+    q3++;
+    p3.innerText = "-1000 Aliens = +50 Aliens/sec: " + q3 + "\n";
+    updateText()
+});
+function updateText() {
+    g.textContent = "Growth Rate : " + mul.toFixed(1) + " Aliens/sec";
+    shop.append(p1);
+    shop.append(p2);
+    shop.append(p3);
+    shop.append(g);
+}
+function checkButton(){
+    if(counter < 10){
         b2.disabled = true;
-    }
-    if(counter>=10){
+    }else{
         b2.disabled = false;
     }
-    requestAnimationFrame(check);
-}
-requestAnimationFrame(check);
-
-b2.addEventListener("click", function () {
-    if(counter >= 10){
-        mul++;
-        counter -= 10;
+    if(counter < 100){
+        b3.disabled = true;
+    }else{
+        b3.disabled = false;
     }
-    g.textContent = "Growth Rate: " + mul;
-    grow.append(g);
-});
+    if(counter < 1000){
+        b4.disabled = true;
+    }else{
+        b4.disabled = false;
+    }
+    requestAnimationFrame(checkButton);
+}
+requestAnimationFrame(checkButton);
