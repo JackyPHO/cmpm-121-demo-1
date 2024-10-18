fa096e3c5bf061ddf727b67b65a2dc33734fcc2f
JackyPHO <100171161+JackyPHO@users.noreply.github.com>
2024-10-18 15:17:19 -0700

    Step 7 Complete

--- src/main.ts
+++ src/main.ts
@@ -30,8 +30,8 @@ let last = performance.now();
 function update(timestamp: number) {
   const delta = timestamp - last;
   last = timestamp;
-  const fps = (1000 / delta);
-  counter = (counter + mul / fps);
+  const fps = 1000 / delta;
+  counter = counter + mul / fps;
   s.textContent = counter.toFixed(5) + " Aliens";
   score.append(s);
   requestAnimationFrame(update);
@@ -48,41 +48,37 @@ const p3 = document.createElement("Price3");
 let q1 = 0;
 let q2 = 0;
 let q3 = 0;
-p1.innerText = "-10 Aliens = +0.1 Aliens/sec: \n";
-p2.innerText = "-100 Aliens = +2.0 Aliens/sec: \n";
-p3.innerText = "-1000 Aliens = +50 Aliens/sec: \n";
+
+//Step 7: Price increases
+let c1 = 10;
+let c2 = 100;
+let c3 = 1000;
 
 const g = document.createElement("Growth");
-g.textContent = "Growth Rate : " + mul.toFixed(1) + " Aliens/sec";
-shop.append(p1);
-shop.append(p2);
-shop.append(p3);
-shop.append(g);
-
+updateText();
 
 const b2 = document.createElement("button");
 b2.className = "button";
 b2.textContent = "🚀";
 grow.appendChild(b2);
 b2.addEventListener("click", function () {
-    mul += 0.1;
-    counter -= 10;
-    q1++;
-    p1.innerText = "-10 Aliens = +0.1 Aliens/sec: " + q1 + "\n";
-    updateText()
+  mul += 0.1;
+  counter -= c1;
+  c1 = c1 * 1.15;
+  q1++;
+  updateText();
 });
-
 
 const b3 = document.createElement("button");
 b3.className = "button";
 b3.textContent = "🛸";
 grow.appendChild(b3);
 b3.addEventListener("click", function () {
-    mul += 2;
-    counter -= 100;
-    q2++;
-    p2.innerText = "-100 Aliens = +2.0 Aliens/sec: " + q2 + "\n";
-    updateText()
+  mul += 2;
+  counter -= c2;
+  c2 = c2 * 1.15;
+  q2++;
+  updateText();
 });
 
 const b4 = document.createElement("button");
@@ -90,35 +86,38 @@ b4.className = "button";
 b4.textContent = "👾";
 grow.appendChild(b4);
 b4.addEventListener("click", function () {
-    mul += 50;
-    counter -= 100;
-    q3++;
-    p3.innerText = "-1000 Aliens = +50 Aliens/sec: " + q3 + "\n";
-    updateText()
+  mul += 50;
+  counter -= c3;
+  c3 = c3 * 1.15;
+  q3++;
+  updateText();
 });
 function updateText() {
-    g.textContent = "Growth Rate : " + mul.toFixed(1) + " Aliens/sec";
-    shop.append(p1);
-    shop.append(p2);
-    shop.append(p3);
-    shop.append(g);
+  p1.innerText = "-" + c1.toFixed(2) + " Aliens = +0.1 Aliens/sec : " + q1 + "\n";
+  p2.innerText = "-" + c2.toFixed(2) + " Aliens = +2.0 Aliens/sec : " + q2 + "\n";
+  p3.innerText = "-" + c3.toFixed(2) + " Aliens = +50 Aliens/sec : " + q3 + "\n";
+  g.textContent = "Growth Rate : " + mul.toFixed(1) + " Aliens/sec";
+  shop.append(p1);
+  shop.append(p2);
+  shop.append(p3);
+  shop.append(g);
 }
-function checkButton(){
-    if(counter < 10){
-        b2.disabled = true;
-    }else{
-        b2.disabled = false;
-    }
-    if(counter < 100){
-        b3.disabled = true;
-    }else{
-        b3.disabled = false;
-    }
-    if(counter < 1000){
-        b4.disabled = true;
-    }else{
-        b4.disabled = false;
-    }
-    requestAnimationFrame(checkButton);
+function checkButton() {
+  if (counter < 10) {
+    b2.disabled = true;
+  } else {
+    b2.disabled = false;
+  }
+  if (counter < 100) {
+    b3.disabled = true;
+  } else {
+    b3.disabled = false;
+  }
+  if (counter < 1000) {
+    b4.disabled = true;
+  } else {
+    b4.disabled = false;
+  }
+  requestAnimationFrame(checkButton);
 }
 requestAnimationFrame(checkButton);
