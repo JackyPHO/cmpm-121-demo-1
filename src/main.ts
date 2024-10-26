2caf4ca758bdd3c9f9d47b7c9eb5f98a21466a8e
JackyPHO <100171161+JackyPHO@users.noreply.github.com>
2024-10-25 20:52:27 -0700

    Step 10 Complete

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ document.title = gameName;
 const header = document.createElement("HIIIII");
 header.innerHTML = gameName;
 app.append(header);
-//Step 1: Adding a click button
 const b1 = document.createElement("button");
 b1.className = "button";
 b1.textContent = "👽";
@@ -44,42 +43,100 @@ const shop: HTMLDivElement = document.querySelector("#shop")!;
 
 //Step 9: Data-driven design
 interface Item {
-    name: string,
-    symbol: string,
-    cost: number,
-    rate: number,
-    button: HTMLButtonElement,
-    tracker: number,
-    text: HTMLElement
-  };
-  
-const availableItems : Item[] = [
-    {name: "🚀 Rocket", symbol: "🚀", cost: 10, rate: 0.1, button: document.createElement("button"), tracker: 0, text: document.createElement("Price")},
-    {name: "🛸 UFO", symbol: "🛸", cost: 100, rate: 2, button: document.createElement("button"), tracker: 0, text: document.createElement("Price")},
-    {name: "👾 Galaxy", symbol: "👾", cost: 1000, rate: 50, button: document.createElement("button"), tracker: 0, text: document.createElement("Price")},
+  name: string;
+  symbol: string;
+  cost: number;
+  rate: number;
+  button: HTMLButtonElement;
+  tracker: number;
+  text: HTMLElement;
+  description: string;
+}
+
+const availableItems: Item[] = [
+  {
+    name: "🚀 Rocket",
+    symbol: "🚀",
+    cost: 10,
+    rate: 0.1,
+    button: document.createElement("button"),
+    tracker: 0,
+    text: document.createElement("Price"),
+    description: "Rockets Should Do the Trick"
+  },
+  {
+    name: "🛸 UFO",
+    symbol: "🛸",
+    cost: 100,
+    rate: 2,
+    button: document.createElement("button"),
+    tracker: 0,
+    text: document.createElement("Price"),
+    description: "Lightning Speed UFO Can Speed Up the Process"
+  },
+  {
+    name: "👾 ET",
+    symbol: "👾",
+    cost: 1000,
+    rate: 50,
+    button: document.createElement("button"),
+    tracker: 0,
+    text: document.createElement("Price"),
+    description: "Giant ET Boss To the Rescue"
+  },
+  {
+    name: "🌌 Universe",
+    symbol: "🌌",
+    cost: 10000,
+    rate: 750,
+    button: document.createElement("button"),
+    tracker: 0,
+    text: document.createElement("Price"),
+    description: "Let's Spread Across the Universe"
+  },
+  {
+    name: "💫 Multiverse",
+    symbol: "💫",
+    cost: 500000,
+    rate: 10000,
+    button: document.createElement("button"),
+    tracker: 0,
+    text: document.createElement("Price"),
+    description: "MULTIVERSE OF MADNESS"
+  }
 ];
 const g = document.createElement("Growth");
-for (const items of availableItems){
-    items.button.className = "button";
-    items.button.textContent = items.name;
-    grow.appendChild(items.button);
-    items.button.addEventListener("click", function(){
-        mul += items.rate;
-        counter -= items.cost;
-        items.cost = items.cost * 1.15;
-        items.tracker++;
-    });
-    function checkButton() {
-        if (counter < items.cost) {
-          items.button.disabled = true;
-        } else {
-          items.button.disabled = false;
-        }
-        items.text.innerText = "-" + items.cost.toFixed(2) + " Aliens = +" + items.rate + " Aliens/sec : " + items.tracker + items.symbol + "\n";
-        shop.append(items.text);
-        g.innerText = "Growth Rate : " + mul.toFixed(1) + " Aliens/sec\n";
-        shop.append(g)
-        requestAnimationFrame(checkButton);
+for (const items of availableItems) {
+  items.button.className = "button";
+  items.button.textContent = items.name;
+  grow.appendChild(items.button);
+  items.button.addEventListener("click", function () {
+    mul += items.rate;
+    counter -= items.cost;
+    items.cost = items.cost * 1.15;
+    items.tracker++;
+  });
+  function checkButton() {
+    if (counter < items.cost) {
+      items.button.disabled = true;
+    } else {
+      items.button.disabled = false;
     }
-    requestAnimationFrame(checkButton);  
+    items.text.innerHTML =
+      "-" +
+      items.cost.toFixed(2) +
+      " Aliens = +" +
+      items.rate +
+      " Aliens/sec : " +
+      items.tracker +
+      items.symbol + 
+      "<br>" +
+      "<i>" + items.description + "<i>" +
+      "<br>" + "<br>";
+    shop.append(items.text);
+    g.innerText = "Growth Rate : " + mul.toFixed(1) + " Aliens/sec\n";
+    shop.append(g);
+    requestAnimationFrame(checkButton);
+  }
+  requestAnimationFrame(checkButton);
 }
