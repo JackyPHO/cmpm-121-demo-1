59226370bd8ef36a1621c5ac851a69f67417a9a1
JackyPHO <100171161+JackyPHO@users.noreply.github.com>
2024-10-30 03:58:02 -0700

    UI change

--- src/main.ts
+++ src/main.ts
@@ -2,59 +2,59 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "Alien Clicker";
+const gameName = "Alien Spawner";
 document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+let counter = 0;
+let growthRate = 0;
 
 function newButton(name: string) {
   const button = document.createElement("button");
-  button.className = "button";
   button.textContent = name;
   return button;
 }
+
+//Main Alien Spawn Button
 const clickButton = newButton("👽");
 app.append(clickButton);
-let growthRate = 0;
-
-const score: HTMLDivElement = document.querySelector("#score")!;
-let counter = 0;
-function roundNumber(num: number): string {
-  return num.toFixed(2).replace(/\.?0+$/, '');
-}
-const count = document.createElement("h2");
-function updateCount() {
-  count.textContent = counter.toFixed(2) + " Aliens";
-  score.append(count);
-}
-
 clickButton.addEventListener("click", function () {
   counter++;
   updateCount();
 });
 
+//Score Display Updated 
+const score: HTMLDivElement = document.querySelector("#score")!;
+const count = document.createElement("h2");
+function updateCount() {
+  count.innerHTML = `${counter.toFixed(2)} 👽<br>${growthRate.toFixed(1)} Aliens / sec`;
+  score.append(count);
+}
 function calculateFPS(timestamp: number, lastFrame: number): number {
   const deltaTime = timestamp - lastFrame;
   const framesPerSecond = 1000 / deltaTime;
   return framesPerSecond;
 }
-
 let last = performance.now();
-function update(timestamp: number) {
+function updateScore(timestamp: number) {
   const fps = calculateFPS(timestamp, last);
   last = timestamp;
   counter += growthRate / fps;
   updateCount();
-  requestAnimationFrame(update);
+  requestAnimationFrame(updateScore);
 }
-requestAnimationFrame(update);
+requestAnimationFrame(updateScore);
 
-const buttonList: HTMLDivElement = document.querySelector("#grow")!;
-const shop: HTMLDivElement = document.querySelector("#shop")!;
+//Alien Shop Button Interface
+const shop = document.getElementById('shop');
 const multiplier = 1.15;
-const growText = document.createElement("h2");
+let colorChange = 138;
+let color = `rgb(74, 189, ${colorChange})`
 
+function roundNumber(num: number): string {
+  return num.toFixed(2).replace(/\.?0+$/, "");
+}
 function getFirstCharacter(str: string): string {
   return Array.from(str)[0];
 }
@@ -64,7 +64,6 @@ interface Item {
   cost: number;
   rate: number;
   tracker: number;
-  text: HTMLElement;
   description: string;
 }
 
@@ -74,7 +73,6 @@ const availableItems: Item[] = [
     cost: 10,
     rate: 0.1,
     tracker: 0,
-    text: document.createElement("h2"),
     description: "Rockets Should Do the Trick",
   },
   {
@@ -82,7 +80,6 @@ const availableItems: Item[] = [
     cost: 100,
     rate: 2,
     tracker: 0,
-    text: document.createElement("h2"),
     description: "UFO Can Speed Up the Process",
   },
   {
@@ -90,7 +87,6 @@ const availableItems: Item[] = [
     cost: 1000,
     rate: 50,
     tracker: 0,
-    text: document.createElement("h2"),
     description: "Giant ET Boss To the Rescue",
   },
   {
@@ -98,7 +94,6 @@ const availableItems: Item[] = [
     cost: 10000,
     rate: 250,
     tracker: 0,
-    text: document.createElement("h2"),
     description: "Let's Spread Across the Universe",
   },
   {
@@ -106,28 +101,42 @@ const availableItems: Item[] = [
     cost: 500000,
     rate: 1000,
     tracker: 0,
-    text: document.createElement("h2"),
     description: "MULTIVERSE OF MADNESS",
   },
 ];
-
-for (const items of availableItems) {
+function createContent(button:HTMLButtonElement, text: HTMLElement){
+  const content = document.createElement('div');
+  content.classList.add('content');
+  text.classList.add('text');
+  content.appendChild(button);
+  content.appendChild(text);
+  return content;
+}
+availableItems.forEach((items) => {
   const shopButton = newButton(items.name);
-  buttonList.appendChild(shopButton);
+  //https://github.com/ishachury20/cmpm-121-demo-1/blob/main/src/main.ts
+  //Background color button change from ishachury20
+  shopButton.style.backgroundColor = color;
+  colorChange += 10;
+  color = `rgb(74, 189, ${colorChange})`;
+  const shopText = document.createElement("div");
   shopButton.addEventListener("click", function () {
     growthRate += items.rate;
     counter -= items.cost;
     items.cost *= multiplier;
     items.tracker++;
   });
-  function checkButton() {
+  const shopItem = createContent(shopButton,shopText);
+  //Updates the item count and shop text based on user input
+  function checkButton(){
     shopButton.disabled = counter < items.cost;
-    items.text.innerHTML = `-${roundNumber(items.cost)} Aliens = +${items.rate} Aliens/sec : ${items.tracker}${getFirstCharacter(items.name)}
-    <br><i>${items.description}</i><br><br>`;
-    shop.append(items.text);
-    growText.innerText = `Growth Rate : ${growthRate.toFixed(1)} Aliens/sec`;
-    shop.append(growText);
+    shopText.innerHTML = `-${roundNumber(items.cost)} Aliens = +${items.rate} Aliens/sec
+    <br><i>${items.description}</i>
+    <br>Total ${items.tracker}${getFirstCharacter(items.name)}`;
     requestAnimationFrame(checkButton);
   }
+  if(shop){
+    shop.appendChild(shopItem)
+  }
   requestAnimationFrame(checkButton);
-}
+});
